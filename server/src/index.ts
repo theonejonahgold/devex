72655a7f8f0a63860c6392847038a91d12edc58c
Jonah Meijers <jonahmeijers97@gmail.com>
2021-04-09 16:59:14 +0200

    feat(server): add firebase

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,15 @@
+import { config } from 'dotenv'
+import { resolve } from 'path'
+config({ path: resolve(__dirname, '..', '.env') })
 import Koa from 'koa'
 import http from 'http'
+import * as admin from 'firebase-admin'
 
 main()
 
 function main() {
+  const app = admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  })
   http.createServer(new Koa().callback()).listen(process.env.PORT || 5000)
 }
